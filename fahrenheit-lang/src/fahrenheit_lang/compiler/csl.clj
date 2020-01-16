7232929eef5bb9251b0e917b1d1667a098ce2f9c
customcommander <hello@spinjs.com>
2020-01-16 13:43:42 +0000

    first cut at transforming an ast into csl syntax

--- fahrenheit-lang/src/fahrenheit_lang/compiler/csl.clj
+++ fahrenheit-lang/src/fahrenheit_lang/compiler/csl.clj
@@ -1,8 +1,31 @@
 (ns fahrenheit-lang.compiler.csl
-  (:require [clojure.zip :as zip]))
+  (:require
+    [clojure.zip :as zip]
+    [clojure.data.xml :as xml]))
+
+(defmulti info-statement->csl #(-> % second keyword))
+
+(defmethod info-statement->csl :title
+  [[_ _ text]]
+  [:title text])
+
+(defmethod info-statement->csl :author
+  [[_ _ name email]]
+  [:author
+    [:name name]
+    [:email email]])
+
+(defn info->csl [ast]
+  (let [loc (zip/vector-zip ast)]
+    (-> loc
+        (zip/down)
+        (zip/right)
+        (zip/edit
+          (fn [[head & tail]]
+            (into [head]
+              (map info-statement->csl tail))))
+        (zip/root))))
 
 (defn transform [ast]
-  (->> ast
-      rest
-      vec
-      (reduce conj)))
+  (-> ast
+      info->csl))
