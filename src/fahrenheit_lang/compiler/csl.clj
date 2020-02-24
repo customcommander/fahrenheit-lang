fa0312c8d94fc30832349b3006a0f9614f8c1cc9
customcommander <hello@spinjs.com>
2020-02-24 12:00:44 +0000

    refactor: reduce boilerplate in csl compiler by introducing hierarchy in multimethods

--- src/fahrenheit_lang/compiler/csl.clj
+++ src/fahrenheit_lang/compiler/csl.clj
@@ -3,79 +3,74 @@
             [clojure.zip :as zip]
             [clojure.string :as str]))
 
-(defmulti ast->xml zip/node)
+(derive ::author ::person)
+
+(derive ::title ::node-with-lang)
+(derive ::title-short ::node-with-lang)
+(derive ::summary ::node-with-lang)
+
+(derive ::url ::link)
+(derive ::documentation ::link)
+(derive ::template ::link)
+
+; I don't like hardcoding the namespace as a string but I couldn't find another way.
+; I tried is to use `(str *ns*)` but that returned "user" instead of "fahrenheit-lang.compiler.csl"!?
+; This is a minor technical debt but I'd like to see this fixed.
+(defn keyword->qualified-keyword [k]
+  (keyword "fahrenheit-lang.compiler.csl" (name k)))
+
+(defmulti ast->xml
+  (fn [loc]
+    (let [node (zip/node loc)]
+      (if (keyword? node)
+        (keyword->qualified-keyword node)
+        node))))
 
 (defmethod ast->xml :default [loc] loc)
 
-(defmethod ast->xml :program [loc]
+(defmethod ast->xml ::program [loc]
   (zip/replace loc :style))
 
-(defmethod ast->xml :about [loc]
+(defmethod ast->xml ::about [loc]
   (zip/replace loc :info))
 
-(defmethod ast->xml :title [loc]
-  (let [[title lang] (zip/rights loc)]
+(defmethod ast->xml ::node-with-lang [loc]
+  (let [node (zip/node loc)
+        [content lang] (zip/rights loc)]
     (if (nil? lang)
       loc
       (-> loc
           (zip/up)
-          (zip/replace [:title {:xml:lang lang} title])
+          (zip/replace [node {:xml:lang lang} content])
           (zip/right)))))
 
-(defmethod ast->xml :title-short [loc]
-  (let [[title lang] (zip/rights loc)]
-    (if (nil? lang)
-      loc
-      (-> loc
-          (zip/up)
-          (zip/replace [:title-short {:xml:lang lang} title])
-          (zip/right)))))
-
-(defmethod ast->xml :summary [loc]
-  (let [[summary lang] (zip/rights loc)]
-    (if (nil? lang)
-      loc
-      (-> loc
-          (zip/up)
-          (zip/replace [:summary {:xml:lang lang} summary])
-          (zip/right)))))
-
-(defmethod ast->xml :url [loc]
-  (let [[uri lang] (zip/rights loc)]
+(defmethod ast->xml ::link [loc]
+  (let [node (zip/node loc)
+        [uri lang] (zip/rights loc)
+        rel (node {:url "self"
+                   :template "template"
+                   :documentation "documentation"})]
     (-> loc
         (zip/up)
-        (zip/replace [:link (merge {:rel "self" :href uri}
-                                   (when-not (nil? lang) {:xml:lang lang}))]))))
+        (zip/replace [:link (merge {:rel rel :href uri}
+                                   (when-not (nil? lang) {:xml:lang lang}))])
+        (zip/right))))
 
-(defmethod ast->xml :documentation [loc]
-  (let [[uri lang] (zip/rights loc)]
-    (-> loc
-        (zip/up)
-        (zip/replace [:link (merge {:rel "documentation" :href uri}
-                                   (when-not (nil? lang) {:xml:lang lang}))]))))
+(defmethod ast->xml ::person [loc]
+  (-> loc
+      (zip/next)
+      (zip/edit #(conj [:name] %))))
 
-(defmethod ast->xml :template [loc]
-  (let [[uri lang] (zip/rights loc)]
-    (-> loc
-        (zip/up)
-        (zip/replace [:link (merge {:rel "template" :href uri}
-                                   (when-not (nil? lang) {:xml:lang lang}))]))))
-
-(defmethod ast->xml :author [loc]
-  (as-> loc l
-        (zip/next l)
-        (zip/edit l #(conj [:name] %))))
-
-(defmethod ast->xml :website [loc]
+(defmethod ast->xml ::website [loc]
   (zip/replace loc :uri))
 
-(defmethod ast->xml :citation-format [loc]
+(defmethod ast->xml ::citation-format [loc]
   (as-> loc l
         (zip/replace l :category)
         (zip/next l)
         (zip/edit l #(assoc {} :citation-format %))))
 
-(defmethod ast->xml :field [loc]
+(defmethod ast->xml ::field [loc]
   (let [field (first (zip/rights loc))
         fields (map str/trim (str/split field #","))]
     (if (= 1 (count fields))
