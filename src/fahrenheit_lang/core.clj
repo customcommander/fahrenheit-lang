a4fb728faf8c46fdbee1e263459f823bb3b48a0a
customcommander <hello@spinjs.com>
2020-05-17 17:34:55 +0000

    add --raw, --ast and --csl as cli options

--- src/fahrenheit_lang/core.clj
+++ src/fahrenheit_lang/core.clj
@@ -1,23 +1,25 @@
 (ns fahrenheit-lang.core
   (:gen-class)
   (:require
-   [clojure.pprint :as pp]
+   [clojure.pprint :refer [pprint]]
    [clojure.tools.cli :refer [parse-opts]]
    [fahrenheit-lang.parser :refer [parse]]
-   [fahrenheit-lang.transform :as ft]
+   [fahrenheit-lang.transform :refer [transform]]
    [fahrenheit-lang.csl :as csl]))
 
 (def cli-options
-  [["-f" "--file FILE" "*.fahr file"]])
+  [["-f" "--file FILE" "Path to *.fahr file"]
+   [nil "--raw" "Parse *.fahr file and print unprocessed AST to standard output."
+    :default false]
+   [nil "--ast" "Parse *.fahr file and print preprocessed AST to standard output."
+    :default false]
+   [nil "--csl" "Generate CSL and print to standard output."
+    :default true]])
 
-(defn -main
-  [& args]
-  (let [{opts :options} (parse-opts args cli-options)]
-    (as-> (:file opts) x
-          (slurp x)
-          (parse x)
-          (do (pp/pprint x) x)
-          (ft/transform x)
-          (do (pp/pprint x) x)
-          (csl/gen-code x)
-          (print x))))
+(defn -main [& args]
+  (let [{opts :options} (parse-opts args cli-options)
+        parsed (-> opts :file slurp parse)]
+    (cond
+      (:raw opts) (-> parsed pprint)
+      (:ast opts) (-> parsed transform pprint)
+      (:csl opts) (-> parsed transform csl/gen-code print))))
